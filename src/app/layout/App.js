3fbe32af82695aad4c41991aac10a945057ff5c4
luciebaker <luciebdev@gmail.com>
2020-01-17 07:50:09 -0500

    first commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gatsby-Events</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
