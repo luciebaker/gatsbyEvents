5b18d1fb4bb460a41fd76d146d78bb2e367c1a85
luciebaker <luciebdev@gmail.com>
2020-01-17 18:14:41 -0500

    router set-up in progress

--- src/features/nav/Navbar/NavBar.js
+++ src/features/nav/Navbar/NavBar.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link } from 'react-router-dom'
 
 class NavBar extends Component {
     render() {
         return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} exact to='/' header>
                     <img src='/assets/logo.png' alt='logo' />
-                     Gatsby Events
+                        Gatsby Events
                 </Menu.Item>
-                <Menu.Item name='Events' />
+                <Menu.Item as={NavLink} to='/events' name='Events' />
+                <Menu.Item as={NavLink} to='/people' name='People' />
                 <Menu.Item>
-                    <Button floated='right' color='teal' content='Create Event' />
+                    <Button as={Link} to='/createEvent' floated='right' color='teal' content='Create Event' />
                 </Menu.Item>
                 <Menu.Item position='right'>
                     <Button standard inverted content='Login' />
