e98d6bae821ac2f7a66ddf9b9de5d5b3450ecda3
luciebaker <luciebdev@gmail.com>
2020-01-18 13:33:27 -0500

    app using Redux

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { createEvent, updateEvent} from '../eventActions'
+import cuid from 'cuid'
 
+const mapState = (state, ownProps) => {
+    const eventId = ownProps.match.params.id
 
-
-class EventForm extends Component {
-    state = {
+    let event = {
         title: '',
         date: '',
         city: '',
         venue: '',
         hostedBy: ''
     }
+
+    if (eventId && state.events.length > 0) {
+        event = state.events.filter(event => event.id === eventId)[0]
+    }
+
+    return {
+        event
+    }
+}
+
+const actions = {
+    createEvent, 
+    updateEvent
+}
+
+class EventForm extends Component {
+    state = {...this.props.event}
 
     componentDidMount() {
         if (this.props.selectedEvent !== null) {
@@ -23,9 +43,16 @@ class EventForm extends Component {
     handleFormSubmit = (evt) => {
         evt.preventDefault()
         if (this.state.id) {
-            this.props.updateEvent(this.state)
+            this.props.updateEvent(this.state);
+            this.props.history.push(`/events/${this.state.id}`)
         } else {
-            this.props.createEvent(this.state)
+            const newEvent = {
+                ...this.state,
+                id: cuid(),
+                hostPhotoURL: "/assets/user.png"
+            }
+            this.props.createEvent(newEvent);
+            this.props.history.push(`/events`)
         }
     }
 
@@ -36,7 +63,6 @@ class EventForm extends Component {
     }
 
     render() {
-        const {cancelFormOpen} = this.props
         const {title, date, city, venue, hostedBy} = this.state
         return (
             <Segment>
@@ -85,10 +111,10 @@ class EventForm extends Component {
                     <Button color="teal" type="submit">
                         Submit
                     </Button>
-                    <Button onClick={cancelFormOpen} type="button">Cancel</Button>
+                    <Button onClick={this.props.history.goBack} type="button">Cancel</Button>
                     </Form>
                 </Segment>
         )
     }
 }
-export default EventForm
+export default connect(mapState, actions)(EventForm)
--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore()
-console.log(store.getState())
 const rootEl = document.getElementById('root');
 
 let render = () => {
     ReactDOM.render(
         <Provider store={store}>
         <BrowserRouter>
+        <ScrollToTop>
         <App />
+        </ScrollToTop>
         </BrowserRouter>
         </Provider>, 
         rootEl) 
