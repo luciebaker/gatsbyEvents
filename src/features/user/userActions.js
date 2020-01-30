8bbbce0247e19df1397a012ed1595dc76a1688ff
luciebaker <luciebdev@gmail.com>
2020-01-30 09:50:37 -0500

    user profile

---
+++ src/features/user/userActions.js
@@ -0,0 +1,15 @@
+import firebase from '../../app/config/firebase';
+import { toastr } from 'react-redux-toastr';
+
+export const updateProfile = (user) =>
+    async (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+        const {isLoaded, isEmpty, ...updatedUser} = user
+        try {
+            await firebase.updateProfile(updatedUser);
+            toastr.success('Success', 'Your profile has been updated')
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
