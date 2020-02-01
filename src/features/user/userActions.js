d421a4ab135d2a16305fcc6d218ec1b42d3c0146
luciebaker <luciebdev@gmail.com>
2020-02-01 16:20:44 -0500

    paging and sorting

--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -1,6 +1,8 @@
 import { asyncActionStart, asyncActionFinish, asyncActionError } from '../async/asyncActions';
 import { toastr } from 'react-redux-toastr';
 import cuid from 'cuid';
+import firebase from '../../app/config/firebase';
+import { FETCH_EVENTS } from '../event/eventConstants';
 
 export const updateProfile = (user) =>
     async (dispatch, getState, {getFirebase}) => {
@@ -75,18 +77,21 @@ export const deletePhoto = (photo) =>
         }
     }
 
-export const setMainPhoto = (photo) => 
-    async (dispatch, getState, {getFirebase}) => {
-        const firebase = getFirebase();
-        try {
+    export const setMainPhoto = photo => async (
+        dispatch,
+        getState,
+        { getFirebase }
+        ) => {
+            const firebase = getFirebase();
+            try {
             return await firebase.updateProfile({
                 photoURL: photo.url
-            })
-        } catch (error) {
+            });
+            } catch (error) {
             console.log(error);
-            throw new Error('Problem setting main photo')
-        }
-    }
+            throw new Error('Problem setting main photo');
+            }
+        };
 
     export const goingToEvent = (event) =>
     async (dispatch, getState, {getFirebase, getFirestore}) => {
@@ -134,3 +139,53 @@ export const setMainPhoto = (photo) =>
             toastr.error('Oops', 'Something went wrong')
         }
     } 
+
+export const getUserEvents = (userUid, activeTab) => 
+    async (dispatch, getState) => {
+        dispatch(asyncActionStart());
+        const firestore = firebase.firestore();
+        const today = new Date(Date.now());
+        let eventsRef = firestore.collection('event_attendee');
+        let query;
+        switch (activeTab) {
+          case 1: // past events
+            query = eventsRef
+                .where('userUid', '==', userUid)
+                .where('eventDate', '<=', today)
+                .orderBy('eventDate', 'desc');
+            break;
+          case 2: // future events
+            query = eventsRef
+                .where('userUid', '==', userUid)
+                .where('eventDate', '>=', today)
+                .orderBy('eventDate');
+            break;
+          case 3: // hosted events
+            query = eventsRef
+                .where('userUid', '==', userUid)
+                .where('host', '==', true)
+                .orderBy('eventDate', 'desc');
+            break;
+            default:
+                query = eventsRef
+                .where('userUid', '==', userUid)
+                .orderBy('eventDate', 'desc');
+        }
+        try {
+            let querySnap = await query.get();
+            let events = [];
+
+            for (let i = 0; i < querySnap.docs.length; i++) {
+                let evt = await firestore
+                    .collection('events')
+                    .doc(querySnap.docs[i].data().eventId)
+                    .get();
+                events.push({...evt.data(), id: evt.id})
+            }
+            dispatch({type: FETCH_EVENTS, payload: {events}})
+            dispatch(asyncActionFinish());
+        } catch (error) {
+            console.log(error);
+            dispatch(asyncActionError());
+        }
+};
